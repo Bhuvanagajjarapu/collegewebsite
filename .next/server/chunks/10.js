3bbec100b39ee1efc45fcdd887646d6d77ff6492
Bhuvanagajjarapu <gajjarapubhuvana@hmail.com>
2024-05-16 23:07:47 +0530

    Fix line endings in node_modules

---
+++ .next/server/chunks/10.js
@@ -0,0 +1,259 @@
+"use strict";
+exports.id = 10;
+exports.ids = [10];
+exports.modules = {
+
+/***/ 8010:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9816);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+const Navbar = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("nav", {
+        className: "jsx-f5332d0f38efc36f" + " " + "navbar",
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: "jsx-f5332d0f38efc36f" + " " + "logo",
+                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_3___default()), {
+                    src: "/logo1.jpeg",
+                    alt: "Logo",
+                    width: 100,
+                    height: 100
+                })
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                className: "jsx-f5332d0f38efc36f" + " " + "nav-list",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                            href: "/Home",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                className: "jsx-f5332d0f38efc36f",
+                                children: "Home"
+                            })
+                        })
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item dropdown",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "#",
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropbtn",
+                                children: "About Us"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropdown-content",
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/about/history",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "College History"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/about/vision",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Vision and Mission"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/about/values",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Values"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item dropdown",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "#",
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropbtn",
+                                children: "Academics"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropdown-content",
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/academics/holidays",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Holidays"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/academics/calendar",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Academic Calendar"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item dropdown",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "#",
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropbtn",
+                                children: "Departments"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropdown-content",
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/departements/cse",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "CSE"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/departements/it",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "IT"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/departements/ai-branches",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "AI BRANCHES"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/departements/mech",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "MECH"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item dropdown",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "#",
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropbtn",
+                                children: "Events"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropdown-content",
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/events/completed",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Completed"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/events/ongoing",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Ongoing"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item dropdown",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "#",
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropbtn",
+                                children: "Faculty"
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: "jsx-f5332d0f38efc36f" + " " + "dropdown-content",
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/faculty/teaching",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Teaching"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                        href: "/faculty/non-teaching",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            className: "jsx-f5332d0f38efc36f",
+                                            children: "Non-Teaching"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                            href: "/branches",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                className: "jsx-f5332d0f38efc36f",
+                                children: "Branches"
+                            })
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                            href: "/contact",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                className: "jsx-f5332d0f38efc36f",
+                                children: "Contact Us"
+                            })
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                        className: "jsx-f5332d0f38efc36f" + " " + "nav-item",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                            href: "/Login",
+                            className: "jsx-f5332d0f38efc36f" + " " + "login-btn",
+                            children: "Login"
+                        })
+                    })
+                ]
+            }),
+            react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default()), {
+                id: "f5332d0f38efc36f",
+                children: ".navbar.jsx-f5332d0f38efc36f{background-color:blue;color:white;padding:10px 20px;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-moz-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.nav-list.jsx-f5332d0f38efc36f{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;list-style-type:none;margin:0;padding:0}.logo.jsx-f5332d0f38efc36f{width:50px;height:auto}.nav-item.jsx-f5332d0f38efc36f{position:relative;margin-left:20px}.nav-item.jsx-f5332d0f38efc36f a.jsx-f5332d0f38efc36f{color:white;padding:16px 20px;text-decoration:none;display:block}.dropbtn.jsx-f5332d0f38efc36f{color:white;padding:16px 20px;text-decoration:none;display:block}.dropdown-content.jsx-f5332d0f38efc36f{display:none;position:absolute;background-color:#f9f9f9;min-width:160px;-webkit-box-shadow:0px 8px 16px 0px rgba(0,0,0,.2);-moz-box-shadow:0px 8px 16px 0px rgba(0,0,0,.2);box-shadow:0px 8px 16px 0px rgba(0,0,0,.2);z-index:1}.dropdown-content.jsx-f5332d0f38efc36f a.jsx-f5332d0f38efc36f{color:black;padding:12px 16px;text-decoration:none;display:block}.dropdown-content.jsx-f5332d0f38efc36f a.jsx-f5332d0f38efc36f:hover{background-color:#ddd}.dropdown.jsx-f5332d0f38efc36f:hover .dropdown-content.jsx-f5332d0f38efc36f{display:block}.login-btn.jsx-f5332d0f38efc36f{background-color:red;color:white;border:1px solid blue;padding:10px 20px;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;-webkit-transition:background-color.3s,color.3s;-moz-transition:background-color.3s,color.3s;-o-transition:background-color.3s,color.3s;transition:background-color.3s,color.3s}.login-btn.jsx-f5332d0f38efc36f:hover{background-color:blue;color:white}"
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Navbar);
+
+
+/***/ })
+
+};
+;
